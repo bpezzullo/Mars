7b4f30845861c861805058105dca83dd4a0e3027
Bill Pezzullo <bpezzullo@gmail.com>
2020-08-21 20:54:05 -0400

    added flask component

---
+++ Mission_to_Mars/app.py
@@ -0,0 +1,114 @@
+
+from flask import Flask, jsonify, render_template, url_for, redirect
+from flask_pymongo import PyMongo
+import mission_to_mars as mtm
+
+import pandas as pd
+from matplotlib import style
+style.use('fivethirtyeight')
+import matplotlib.pyplot as plt
+
+#################################################
+# Database Setup
+#################################################
+#engine = create_engine("sqlite:///Resources/hawaii.sqlite")
+
+# reflect an existing database into a new model
+#Base = automap_base()
+# reflect the tables
+#Base.prepare(engine, reflect=True)
+
+# Save reference to the table
+#Station = Base.classes.station
+#Measurement = Base.classes.measurement
+
+#################################################
+# Flask Setup
+#################################################
+app = Flask(__name__)
+
+# Use PyMongo to establish Mongo connection
+mongo = PyMongo(app, uri="mongodb://localhost:27017/Mars_app")
+
+#################################################
+# Flask Routes
+#################################################
+
+
+@app.route("/")
+@app.route("/home")
+def welcome():
+    # pull information from the mongo db
+    pictures = mongo.db.mars_pic.find_one()
+    news = mongo.db.mars_news.find_one()
+    images = mongo.db.mars_img.find_one()
+    
+    return render_template('index.html', pictures=pictures, news=news, mars_img=images)
+
+
+@app.route("/about")
+def about():
+
+    images = mongo.db.mars_img.find_one()
+    return render_template('about.html', title='About', mars_img=images)
+
+@app.route("/data")
+def data():
+
+    images = mongo.db.mars_img.find_one()
+    data = mongo.db.mars_data.find_one()
+    html_string = data['html']
+
+    return render_template('data.html', title='Mars Data', mars_img=images, data=html_string)
+
+
+@app.route("/scrape_feature_image")
+def scrape_feature_image():
+
+    # Run the scrape function
+    img_data = mtm.scrape_feature_image()
+
+    # Update the Mongo database using update and upsert=True
+    mongo.db.mars_img.update({}, img_data, upsert=True)
+
+    # Redirect back to home page
+    return redirect("/")
+
+@app.route("/scrape_latest_news")
+def scrape_latest_news():
+
+    # Run the scrape function
+    news = mtm.scrape_latest_news()
+
+    # Update the Mongo database using update and upsert=True
+    mongo.db.mars_news.update({}, news, upsert=True)
+
+    # Redirect back to home page
+    return redirect("/")
+
+@app.route("/scrape_mars_data")
+def scrape_mars_data():
+    # Run the scrape function
+    mars_data = mtm.scrape_mars_data()
+
+    # Update the Mongo database using update and upsert=True
+    mongo.db.mars_data.update({}, mars_data, upsert=True)
+
+    # Return template and data
+    return redirect("/data")
+
+@app.route("/scrape_mars_hemi")
+def scrape_mars_hemi():
+
+    # Run the scrape function
+    hemisphere = mtm.scrape_mars_hemi()
+
+    # Update the Mongo database using update and upsert=True
+    mongo.db.mars_pic.update({}, hemisphere, upsert=True)
+
+    # Redirect back to home page
+    return redirect("/")
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
---
+++ Mission_to_Mars/mission_to_mars.py
@@ -0,0 +1,146 @@
+from splinter import Browser
+from bs4 import BeautifulSoup
+import requests
+import pymongo
+import time
+import pandas as pd
+
+def init_browser():
+    # @NOTE: Replace the path with your actual path to the chromedriver
+    executable_path = {"executable_path": "chromedriver.exe"}
+    return Browser("chrome", **executable_path, headless=False)
+
+
+
+# # Grab the latest news article from the website -'https://mars.nasa.gov/news/'
+# ## store the title and paragraph description.
+
+def scrape_latest_news():
+
+    # set the URL 
+    url = 'https://mars.nasa.gov/news/'
+
+    # Retrieve page with the requests module
+    response = requests.get(url)
+    # Create BeautifulSoup object; parse with 'lxml'
+    soup = BeautifulSoup(response.text, 'lxml')
+
+    # print(soup.prettify())
+
+    # results are returned as an iterable list
+    results = soup.body.find('div',class_='slide')
+
+    news_p = results.find('div',class_='rollover_description_inner').text
+    news_title=results.find('div',class_='content_title').text
+
+    news = {'title' : news_title, 
+            'text' : news_p}
+
+    return (news)
+
+# # Scrape the featured image from the website - https://www.jpl.nasa.gov/spaceimages/?search=&category=Mars'
+# ## Store the image and create an url to the image.
+
+def scrape_feature_image():
+
+    browser = init_browser()
+
+    url = 'https://www.jpl.nasa.gov/spaceimages/?search=&category=Mars'
+    browser.visit(url)
+
+    browser.click_link_by_partial_text('FULL IMAGE')
+    browser.click_link_by_partial_text('more info')
+
+    html = browser.html
+    soup = BeautifulSoup(html, 'lxml')
+    results = soup.find('section',class_='content_page module').find('figure',class_='lede').find('a')['href']
+
+    feaured_image_url = {'url' : 'https://www.jpl.nasa.gov' + results}
+
+    
+    # Quite the browser after scraping
+    browser.quit()
+
+    return feaured_image_url
+
+
+# # Scrape the Mars facts from https://space-facts.com/mars/
+# ## Place them into a dataframe to store in an html file.
+
+def scrape_mars_data():
+
+    url = 'https://space-facts.com/mars/'
+
+    tables = pd.read_html(url)
+    tables
+
+    df = tables[0]
+
+    # Export the city data into an html file
+    df.to_html(open('Resources/mars_facts.html', 'w', encoding="utf-8"))
+
+    with open('Resources/mars_facts.html', 'r') as file:
+        data = file.read()
+    
+    data_dict = {'html' : data}
+
+    return data_dict
+
+
+# # Scrape the website -  for the 4 urls images
+# ## https://astrogeology.usgs.gov/search/results?q=hemisphere+enhanced&k1=target&v1=Mars
+
+def scrape_mars_hemi():
+    url='https://astrogeology.usgs.gov/search/results?q=hemisphere+enhanced&k1=target&v1=Mars'
+    # Retrieve page with the requests module
+    response = requests.get(url)
+    # Create BeautifulSoup object; parse with 'lxml'
+    soup = BeautifulSoup(response.text, 'lxml')
+
+    # print(soup.prettify())
+
+    results = soup.body.find_all('div',class_='item')
+
+    browser = init_browser()
+
+    hemisphere_image_urls = []
+
+    for result in results:
+
+        # Error handling
+        try:
+        
+            # Identify and return title of listing
+            title = result.find('div',class_='description').find('h3').text
+
+            browser.visit(url)
+        
+            time.sleep(1)
+        
+            browser.click_link_by_partial_text(title)
+            html = browser.html
+            soup = BeautifulSoup(html, 'lxml')
+            link = soup.body.find('div',class_='container').find('div',class_='downloads').li.a['href']
+
+            # Run only if title, and link are available
+            if (title and link):
+
+                # Dictionary to be inserted as a MongoDB document
+                post = {
+                    'title': title,
+                    'url': link
+                    }
+
+                hemisphere_image_urls.append(post)
+            
+        except Exception as e:
+            print(e)
+
+    
+    # Quite the browser after scraping
+    browser.quit()
+
+    return hemisphere_image_urls
+
+
+
